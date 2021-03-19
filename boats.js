ce30f6653599226d3cf8e298f29aea9a4e5ac0e0
Kyle <kylespinaio@hotmail.com>
2021-03-19 19:01:47 -0400

    added requirments to schema to buildout model

--- boats.js
+++ boats.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
 const boatSchema = new mongoose.Schema({
-  manufacturer: String,
-  model: String,
-  year: Number,
-  length: Number,
+  manufacturer: {type: String, required: true, minlength: 2, maxlength: 100},
+  model: {type: String, required: true, minlength: 2, maxlength: 100},
+  year: {type: Number, required: true, minlength: 2, maxlength: 4},
+  length: {type: Number, required: true, minlength: 2, maxlength: 3},
   beam: Number,
   balastRatio: Number,
   sailArea: Number,
   displacmentLength: Number,
   capsize: Number,
+  motorType: String,
   
 });
