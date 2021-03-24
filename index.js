e60c9a36c8bba1053fd1fcbdf665d8ebf874ce1c
Kyle <kylespinaio@hotmail.com>
2021-03-23 23:00:56 -0400

    added express to server code. began structure again

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const connectDB = require('./startup/db')
-const boats = require('./routes/boats')
+// const connectDB = require('./startup/db')
+// const boats = require('./routes/boats')
 
-connectDB()
+// connectDB()
 
-app.use(express.json());
-app.use('/api/boats', boats);
+// app.use(express.json());
+// app.use('/api/boats', boats);
 
 
-const port = process.env.PORT || 5000;
-app.listen(port,() => {
-  console.log(`Server started on port: ${port}`)
+// const port = process.env.PORT || 5000;
+// app.listen(port,() => {
+//   console.log(`Server started on port: ${port}`)
 
-});
+// });
 
+
+const express = require('express');
+
+const app = express();
+
+
+app.listen(300, function (){
+    console.log('server has started. listening on port 3000...')
+})
